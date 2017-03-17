b1f24508eece38c589192f0f7ac3c3cebdc7cf10
yibn2008 <yibn2008@qq.com>
2017-03-17 12:44:47 +0800

    test: finish unit test

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
 # css-split
 
 split css into different parts by selector number
+
+## Usage
+
+Split css into different parts by selectors size:
+
+```js
+const split = require('css-split')
+
+const css = `
+// some css content
+@charset "UTF-8";
+.ui-warp .ui-title-warp {
+  margin: 20px 0 10px;
+}
+
+.ui-warp .ui-title-cont,
+.ui-warp .ui-title-cont x {
+  font-size: 20px;
+}
+
+.ui-warp .ui-title-cont a {
+  font-size: 14px;
+  margin-left: 10px;
+}
+
+.ui-warp .ui-task-desc .add-row-btn {
+  margin-left: 10px;
+}
+
+//......
+`
+
+let parts = split(css, 2)
+for (let part of parts) {
+  console.log('count:', part.count) // selectors number
+  console.log(part.content) // split content
+}
+```
+
+Also you can count selectors number of css:
+
+```js
+const split = require('css-split')
+
+const css = `
+// some css content
+@charset "UTF-8";
+.ui-warp .ui-title-warp {
+  margin: 20px 0 10px;
+}
+
+.ui-warp .ui-title-cont,
+.ui-warp .ui-title-cont x {
+  font-size: 20px;
+}
+`
+// count = 3
+let count = split.count(css)
+```
+
+## API
+
+### `split(content, size)`
+
+- `content`, *{String}*, css content
+- `size`, *{Number}*, selectors number to split
+
+split `content` by `size`, and return the parts `Array`:
+
+```js
+[{
+  id: 0,
+  count: 111,
+  from: 0,
+  to: 1212,
+  content: 'split css rules ...'
+}, ...]
+```
+
+### `count(content)`
+
+- `content`, *{String}*, css content
+
+count all the selectors number of `content`, and return a `Number`
+
+## LICENSE
+
+MIT
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,67 @@
 'use strict'
 
+const fs = require('fs')
+const path = require('path')
 const assert = require('assert')
+const postcss = require('postcss')
+const split = require('..')
 
-describe('test', function () {
-  it('should do something', function () {
-    // TODO
+const fixturesDir = path.join(__dirname, 'fixtures')
+
+function countNode (node) {
+  if (node.type === 'rule') {
+    return node.selectors.length
+  }
+  if (node.type === 'atrule' && node.nodes) {
+    return 1 + node.nodes.reduce((memo, n) => {
+      return memo + countNode(n)
+    }, 0)
+  }
+  return 0
+}
+
+const plugin = postcss.plugin('postcss-count', () => {
+  return (css, result) => {
+    result.count = 0
+
+    for (let node of css.nodes) {
+      result.count += countNode(node)
+    }
+  }
+})
+
+describe('test css-split', function () {
+  it('should able to split css', function * () {
+    let files = [
+      // name, size
+      ['next.css', 1000],
+      ['next.min.css', 1000],
+      ['broken.css', 5]
+    ]
+
+    for (let pair of files) {
+      // split next.css
+      let name = pair[0]
+      let size = pair[1]
+      let all = fs.readFileSync(path.join(fixturesDir, name), 'utf8').trim()
+      let parts = split(all, size)
+      let contents = []
+
+      for (let part of parts) {
+        // able to parse, means syntax is correct
+        let count = (yield postcss([plugin()]).process(part.content)).count
+        assert(part.count <= size, `need ${part.count} <= ${size}}`)
+        assert(count <= size, `need ${count} <= ${size}}`)
+        contents.push(part.content)
+      }
+
+      assert.equal(contents.join(''), all)
+    }
+  })
+
+  it('should able to count css', function () {
+    let content = fs.readFileSync(path.join(fixturesDir, 'next.css'), 'utf8')
+
+    assert.equal(split.count(content), 4980)
   })
 })
